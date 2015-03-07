c15eea38707571b79eb5e4ffa0f3f1c26909d6bd
Malik717 <rwaltasti@gmail.com>
2015-03-07 16:29:40 -0600

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,26 @@
-alert("hello world");
+//Get income twice per second
+setInterval(getIncome(), 500);
+var income;
+var eff;
+
+//Get the current income
+function getIncome(){
+  var money = document.getElementById("resource-money")
+  var temp = money.innerText.substring(money.innerText.indexOf("(")+1);
+  income = temp.substring(0, temp.indexOf("/"))
+}
+
+//Loop all buildings that can be build and get their elements
+function getEff(){
+  var temp = new Array();
+  var elem = document.getElementsByClassName("thing button building nopic  disabled");
+  for(var i = 0; i <=elem.length; i++){
+    temp.push(elem[i].getElementsByClassName("amount")(0).innerText);
+  
+  
+  
+  }
+}
+
+
+
